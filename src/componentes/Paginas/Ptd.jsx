d99fa12ac32522d1fe80a9ee5643988f056ed35d
Juan Felipe Fonseca Vargas <juanffonsecav5@gmail.com>
2024-07-30 17:01:45 -0500

    Se agrego la PTD en texto

--- src/componentes/Paginas/Ptd.jsx
+++ src/componentes/Paginas/Ptd.jsx
@@ -2,6 +2,154 @@ import "./EstilosPaginas/PtdDocs.css";
 
 export default function Ptd() {
   return (
-    <div className="PtdDiv"><iframe className="PtdDocs" src="https://docs.google.com/document/d/e/2PACX-1vS8amJX2BxATiCWSmeZ7Hbpv5Zsasgy_qVA3DGJvVrayLRkdCtzZ3lYIl6lp7ePuNDBBhhQHcVck3ZD/pub?embedded=true"></iframe></div>
+      <div className="PtdSection">
+        <div>
+          <div className="callcenterBox">
+            <h1 className="titles">Política de tratamiento de datos Integral Asesores de seguros</h1>
+          </div>
+          <div className="justify">
+            <p>Para garantizar la privacidad y seguridad de la información personal de nuestros clientes y colaboradores, es crucial establecer una política robusta de protección de datos. Esta política se rige por la Ley 1581 del 2012 en Colombia, la cual tiene como objetivo proteger los derechos de las personas sobre la información que se recopila, almacena, utiliza, circula y suprime. Al cumplir con estas normativas legales, fortalecemos la confianza y credibilidad en nuestra empresa, demostrando nuestro compromiso con el respeto a la privacidad y el uso adecuado de la información.</p>
+          </div>
+        </div>
+        <hr />
+        <div>
+          <div className="callcenterBox">
+            <h3 className="titles">Datos de contacto</h3>
+          </div>
+          <ul>
+            <li>Razón Social : Integral Asesores de Seguros </li>
+            <li>NIT: 830008194-8</li>
+            <li>Dirección: Carrera 31A #25A-93</li>
+            <li>Correo electrónico: asesor@integraldeseguros.com.co</li>
+            <li>Teléfono: 3684136 ext: 123</li>
+            <li>Celular: 3133331128</li>
+            <li>Pagina Web: https://integraldeseguros.com/</li>
+          </ul>
+        </div>
+        <hr />
+        <div>
+          <div className="callcenterBox">
+            <h3 className="titles">Definiciones</h3>
+          </div>
+          <p>Para efectos de la presente política de tratamiento de datos de la empresa se entiende por: </p>
+          <ul>
+            <li>Autorización: Consentimiento previo, expreso e informado del Titular para llevar a cabo el Tratamiento de datos personales.</li>
+            <li>Base de Datos: Conjunto organizado de datos personales que sea objeto de tratamiento.</li>
+            <li>Datos personales: Cualquier información vinculada o que pueda asociarse a una o varias personas naturales determinadas o determinables.</li>
+            <li>Encargado del Tratamiento: Persona natural o jurídica, pública o privada, que por sí misma o en asocio con otros, realice el Tratamiento de datos personales por cuenta del Responsable del Tratamiento.</li>
+            <li>Responsable del Tratamiento: Persona natural o jurídica, pública o privada, que por sí misma o en asocio con otros, decida sobre la base de datos y/o el tratamiento de los datos.</li>
+            <li>Titular: Persona natural cuyos datos personales sean objeto de tratamiento.</li>
+            <li>Tratamiento: Cualquier operación o conjunto de operaciones sobre datos personales, tales como la recolección, almacenamiento, uso, circulación o supresión.</li>
+          </ul>
+        </div>
+        <hr />
+        <div>
+          <div className="callcenterBox">
+            <h3 className="titles">Principios para el Tratamiento de datos personales.</h3>
+          </div>
+          <p>En el desarrollo, interpretación y aplicación de la presente política, se aplicarán, de manera armónica e integral, los siguientes principios:</p>
+          <ul>
+            <li>Principio de legalidad en materia de Tratamiento de datos: El Tratamiento a que se refiere la presente política es una actividad reglada que debe sujetarse a lo establecido en ella y en las demás disposiciones que la desarrollen.</li>
+            <li>Principio de finalidad: El Tratamiento debe obedecer a una finalidad legítima de acuerdo con la Constitución y la Ley 1581 del 2012, la cual debe ser informada al Titular.</li>
+            <li>Principio de libertad: El Tratamiento sólo puede ejercerse con el consentimiento, previo, expreso e informado del titular. Los datos personales no podrán ser obtenidos o divulgados sin previa autorización, o en ausencia de mandato legal o judicial que releve el consentimiento.</li>
+            <li>Principio de veracidad o calidad: La información sujeta a tratamiento debe ser veraz, completa, exacta, actualizada, comprobable y comprensible. Se prohíbe el tratamiento de datos parciales, incompletos, fraccionados o que induzcan a error.</li>
+            <li>Principio de transparencia: En el tratamiento debe garantizarse el derecho del titular a obtener del responsable del tratamiento o del encargado del tratamiento, en cualquier momento y sin restricciones, información acerca de la existencia de datos que le concierne.</li>
+            <li>Principio de acceso y circulación restringida: El tratamiento se sujeta a los límites que se derivan de la naturaleza de los datos personales, de las disposiciones de la presente política , la constitución y la ley 1581 del 2012. En este sentido, el tratamiento sólo podrá hacerse por personas autorizadas por el titular y/o por las personas previstas en la presente ley.</li>
+          </ul>
+          <p>Los datos personales, salvo la información pública, no podrán estar disponibles en Internet u otros medios de divulgación o comunicación masiva, salvo que el acceso sea técnicamente controlable para brindar un conocimiento restringido sólo a los titulares o terceros autorizados conforme a la presente ley.</p>
+          <ul>
+            <li>Principio de seguridad: La información sujeta a tratamiento por el responsable del tratamiento o encargado del tratamiento a que se refiere la presente política, se deberá manejar con las medidas técnicas, humanas y administrativas que sean necesarias para otorgar seguridad a los registros evitando su adulteración, pérdida, consulta, uso o acceso no autorizado o fraudulento.</li>
+            <li>Principio de confidencialidad: Todas las personas que intervengan en el tratamiento de datos personales que no tengan la naturaleza de públicos están obligadas a garantizar la reserva de la información, inclusive después de finalizada su relación con alguna de las labores que comprende el tratamiento, pudiendo sólo realizar suministro o comunicación de datos personales cuando ello corresponda al desarrollo de las actividades autorizadas en la presente ley y en los términos de la misma.</li>
+          </ul>
+        </div>
+        <hr />
+        <div>
+          <div className="callcenterBox">
+            <h3 className="titles">Categorías especiales de datos</h3>
+          </div>
+          <p>Para los propósitos de la presente política, se da a entender como datos sensibles a aquellos que puedan terminar afectando la intimidad del titular tales como su nombre, dirección, celular , correo electrónico, cédula, conformación del grupo familiar, datos financieros , médicos y/o biometricos. </p>
+        </div>
+        <hr />
+        <div>
+          <div className="callcenterBox">
+            <h3 className="titles">Tratamiento de datos sensibles</h3>
+          </div>
+          <p>Para cada uno de los titulares se le da un tratamiento diferente el cual para esta politica aplica de la siguiente manera: </p>
+          <ul>
+            <li>Socios: El tratamiento de datos suministrados por nuestros colaboradores serán necesarios en el caso de necesitar hacer un contacto , comunicación , reconocimiento  , ejercicio o defensa de un derecho en un proceso judicial.</li>
+            <li>Colaboradores: El tratamiento de datos suministrados por nuestros colaboradores serán necesarios en el caso de necesitar hacer un contacto , comunicación , reconocimiento  , ejercicio o defensa de un derecho en un proceso judicial.  </li>
+            <li>Clientes: El tratamiento de datos suministrados por nuestros clientes tendrán la finalidad del conocimiento de información básica , para el asesoramiento, cotización ,ofrecimiento y cobro de las pólizas manejadas por las aseguradoras con las que trabaja la agencia .Además de en caso de que sea necesario el ejercicio o defensa de un derecho en un proceso judicial</li>
+            <li>Proveedores: El tratamiento de datos suministrados por nuestros proveedores tendrán la finalidad de hacer comunicaciones (tales como solicitudes, cobros, requerimientos, etc…), reconocimiento  , ejercicio o defensa de un derecho en un proceso judicial. </li>
+          </ul>
+        </div>
+        <hr />
+        <div>
+          <div className="callcenterBox">
+            <h3 className="titles">Derechos de los titulares</h3>
+          </div>
+          <p> El titular de los datos personales tendrá los siguientes derechos:</p>
+          <ul>
+            <li>Conocer, actualizar y rectificar sus datos personales frente a los responsables del tratamiento o encargados del tratamiento. Este derecho se podrá ejercer, entre otros, frente a datos parciales, inexactos, incompletos, fraccionados, que induzcan a error, o aquellos cuyo tratamiento esté expresamente prohibido o no haya sido autorizado.</li>
+            <li>Solicitar prueba de la autorización otorgada al responsable del tratamiento.</li>
+            <li>Ser informado por el responsable del tratamiento o el encargado del tratamiento, previa solicitud, respecto del uso que le ha dado a sus datos personales.</li>
+            <li>Presentar ante la Superintendencia de Industria y Comercio quejas por infracciones a lo dispuesto en la presente política y las demás normas que la modifiquen, adicionen o complementen.</li>
+            <li>Revocar la autorización y/o solicitar la supresión del dato cuando en el tratamiento no se respeten los principios, derechos y garantías constitucionales y legales. La revocatoria y/o supresión procederá cuando la Superintendencia de Industria y Comercio haya determinado que en el tratamiento el responsable o encargado han incurrido en conductas contrarias a esta política y a la constitución.</li>
+            <li>Acceder en forma gratuita a sus datos personales que hayan sido objeto de tratamiento.</li>
+          </ul>
+        </div>
+        <hr />
+        <div>
+          <div className="callcenterBox">
+            <h3 className="titles">Canales de contacto</h3>
+          </div>
+          <p>Conociendo los derechos que posee el titular de los datos disponemos para cualquier tipo de petición , queja o reclamo sobre el conocimiento los datos , nuestra página web , la linea de WhatsApp la cual corresponde al número celular que se encuentra en la sección de datos de contacto o el correo electrónico ubicado en la sección previamente mencionada. La consulta será atendida en un término máximo de diez (10) días hábiles contados a partir de la fecha de recibo de la misma. Cuando no fuere posible atender la consulta dentro de dicho término, se informará al interesado, expresando los motivos de la demora y señalando la fecha en que se atenderá su consulta, la cual en ningún caso podrá superar los cinco (5) días hábiles siguientes al vencimiento del primer término.</p>
+        </div>
+        <hr />
+        <div>
+          <div className="callcenterBox">
+            <h3 className="titles">Medidas de seguridad </h3>
+          </div>
+          <p>Para poder garantizar la seguridad de los datos de nuestros clientes , colaboradores, socios y proveedores manejamos unas medidas para que no haya ningún tipo de vulnerabilidad por parte de terceros no autorizados las cuales son: </p>
+          <ul>
+            <li>Control de acceso: Limitar el acceso a los datos sólo a las personas autorizadas y garantizar que cada usuario tenga los permisos adecuados según su función en la organización.</li>
+            <li>Encriptación: Encriptar los datos tanto en reposo (almacenados) como en tránsito (en movimiento) para protegerlos de accesos no autorizados.</li>
+            <li>Respaldo de datos: Realizar copias de seguridad periódicas de los datos y almacenarlas de manera segura, preferiblemente en ubicaciones fuera del sitio principal.</li>
+            <li>Actualizaciones y parches: Mantener actualizados los sistemas y aplicaciones utilizados para el tratamiento de datos, e implementar los parches de seguridad necesarios para protegerse contra vulnerabilidades conocidas.</li>
+            <li>Monitoreo y auditoría: Supervisar activamente el acceso y uso de los datos, periódicamente para detectar posibles brechas de seguridad o actividades sospechosas.</li>
+            <li>Formación y concienciación: Capacitar a los empleados sobre las políticas de seguridad de la información y fomentar una cultura de seguridad en la organización, incluyendo la sensibilización sobre prácticas seguras de manejo de datos y la detección de posibles amenazas como el phishing.</li>
+            <li>Seguridad física: Proteger físicamente los equipos y servidores que almacenan datos sensibles, asegurando que solo personal autorizado pueda acceder a ellos.</li>
+          </ul>
+          <p>Estas medidas son fundamentales para proteger la confidencialidad, integridad y disponibilidad de la información, mitigando riesgos como el acceso no autorizado, la pérdida de datos o la manipulación maliciosa.
+          </p>
+        </div>
+        <hr />
+        <div>
+          <div className="callcenterBox">
+            <h3 className="titles">Deberes de los responsables del tratamiento</h3>
+          </div>
+          <p>Como responsables del tratamiento de datos cumplimos los siguientes deberes sin perjuicio de las demás disposiciones previstas en la presente política y en otras que rijan  su actividad:</p>
+          <ul>
+            <li>Garantizar al titular, en todo momento, el pleno y efectivo ejercicio del derecho de hábeas data.</li>
+            <li>Solicitar y conservar copia de la respectiva autorización otorgada por el titular para el tratamiento.</li>
+            <li>Informar debidamente al titular sobre la finalidad de la recolección y los derechos que tiene en virtud de la autorización otorgada.</li>
+            <li>Conservar la información bajo las condiciones de seguridad necesarias para impedir su adulteración, pérdida, consulta, uso o acceso no autorizado o fraudulento.</li>
+            <li>Garantizar que la información sea veraz, completa,exacta, actualizada, comprobable y comprensible.</li>
+            <li>Actualizar oportunamente la información,atendiendo de esta forma todas las novedades respecto de los datos del titular. Adicionalmente, se deberán implementar todas las medidas necesarias para que la información se mantenga actualizada.</li>
+            <li>Rectificar la información cuando sea incorrecta y comunicar lo pertinente.</li>
+            <li>Respetar las condiciones de seguridad y privacidad de la información del titular.</li>
+            <li>Tramitar las consultas y reclamos formulados en los términos señalados por la ley.</li>
+          </ul>
+        </div>
+        <hr />
+        <div>
+          <div className="callcenterBox">
+            <h3 className="titles">Vigencia </h3>
+          </div>
+          <p>Nuestra política de tratamiento de datos tiene una vigencia permanente y se mantendrá actualizada conforme a las disposiciones legales vigentes y las mejores prácticas en materia de protección de datos. Esta vigencia asegura que la política esté alineada con los cambios en la normativa y las necesidades de la organización, garantizando la continuidad en la protección de la información y el cumplimiento de los derechos de los titulares de los datos.</p>
+          <p>Además, nuestra política contempla la revisión periódica y la actualización de medidas de seguridad, procedimientos y controles internos para garantizar la confidencialidad, integridad y disponibilidad de la información, así como para prevenir cualquier tipo de acceso no autorizado o uso indebido de los datos.
+          </p>
+          <p>Con esta política de tratamiento de datos en constante evolución, reafirmamos nuestro compromiso con la protección de la privacidad y los derechos de las personas, cumpliendo con los estándares legales y éticos en el manejo de la información personal.</p>
+        </div>
+      </div>
   )
 }
